e49b87d9bb2febd8e0e178a852f763a461782470
Nick Drewe <nick.drewe@gmail.com>
2015-03-27 16:11:06 +0700

    Location search + spam filter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var ig = require('instagram-node').instagram();
 var lessMiddleware = require('less-middleware');
+var _ = require('lodash');
 
 // ig.use({ access_token: 'YOUR_ACCESS_TOKEN' });
 ig.use({ client_id: '3d7bf45865124de8b0a8d8ddca775b2f', client_secret: '73b72e3dd1c34bb8ae45e9e1b64dfebb' });
@@ -35,39 +36,148 @@ setInterval(function(){
 function everyMinute(){
 
 	var i = 0;
-	var pages = 3;
-	var opt = { count: 33 };
+	var j = 0;
+	var pages = 5;
+	var optTag = { count: 33 };
+	var optLoc = { count: 33, distance: 2000 };
 
 	posts = [];
 
-	updatePosts(opt);
+	updatePostsTag(optTag);
+	updatePostsLoc(optLoc);
 
-	function updatePosts(opt){
-		ig.tag_media_recent('coachella', opt, function(err, medias, pagination, remaining, limit) {
+	function updatePostsTag(optTag){
+		ig.tag_media_recent('coachella', optTag, function(err, medias, pagination, remaining, limit) {
 			//add media to posts
 			medias.forEach(function(media){
-
-				//Buid post object
-				var post = {
-					link: media.link,
-					type: media.type,
-					images: media.images,
-					videos: media.videos
-				}
-				//Push to posts array
-				posts.push(post);
+				//Push to array
+				pushPost(media);
 			});
 
 			i++;
 			//Loop if less than 3 pages
 			if(i < pages){
-				opt = { count: 33, max_id: pagination.next_max_id };
-				updatePosts(opt);
+				optTag = { count: 33, max_id: pagination.next_max_id };
+				updatePostsTag(optTag);
 				//after 3 pages:
 			} else {
 				console.log('Posts updated: ' + posts.length);
 			}
 		});
+	}
+
+	function updatePostsLoc(optLoc){
+		ig.media_search(33.682198, -116.238262, optLoc, function(err, medias, remaining, limit) {
+			//add media to posts
+			medias.forEach(function(media){
+				//Push to array
+				pushPost(media);
+			});
+
+			j++;
+			//Loop if less than 3 pages
+			if(j < pages){
+				//use min created_time for pagination
+				var times = _.pluck(medias, 'created_time');
+				var minTime = Math.min.apply(Math, times);
+				//max_timestamp for next request is min timestamp from previous
+				optLoc = { count: 25, distance: 2000, max_timestamp: minTime };
+				updatePostsTag(optLoc);
+				//after 3 pages:
+			} else {
+				console.log('Posts updated: ' + posts.length);
+			}
+		});
+	}
+
+	function pushPost(media){
+		//prevent duplicates in posts
+		var links = _.pluck(posts, 'link');
+		if(!_.includes(links, media.link)){
+			//Buid post object
+			//console.log(media);
+
+			var post = {
+				username: media.user.username,
+				link: media.link,
+				type: media.type,
+				images: media.images,
+				videos: media.videos
+			}
+
+			if(media.caption && media.caption.text){
+				post.caption = media.caption.text;
+			}
+
+			if(media.location && media.location.latitude){
+				post.location = media.location;
+			}
+			//Check for spam
+			if(!isSpam(post)){
+				//Push to posts array
+				posts.push(post);
+			}
+		}
+	}
+}
+
+var spamAccounts = [
+	'harlemragshop1934',
+	'ueksszkud',
+	'srbhcocjy',
+	'uyjhvdhxa',
+	'rufuuutjg',
+	'kkvzfatsy',
+	'qwsebxgti',
+	'cielolaaa',
+	'flzxiweph',
+	'kyjgepkww',
+	'pjdombykv',
+	'vjvldrttm',
+	'zjbuwbgyu',
+	'goqjmjzlk',
+	'chmacphsj',
+	'jtmanalex',
+	'sammiesheadwear',
+	'zgueqckdp',
+	'stylenistaboutique',
+	'la_edm_designs',
+	'zebedesign',
+	'echbritwf',
+	'hzzwcxjjc',
+	'cdkizunux',
+	'mgnrjxzse',
+	'klvzyhcvh',
+	'aavhfckrr',
+	'wanderlustcouture',
+	'atxogoper',
+	'happinessbrand',
+	'wanderlustcouture',
+	'shophomegrowndesigns',
+	'wanderlustcouture',
+	'rave_naked',
+	'hbimcklee',
+	'artistexpressions',
+	'pistolandlucy',
+	'bellexo_boutique',
+	'graciejiujitsupd',
+	'qwsebxgti',
+	'orflagapop'
+
+
+];
+
+var spamStrings = /(my bio|8613566347511|contest|coupon)/;
+
+function isSpam(post){
+	if (post.caption && spamStrings.test(post.caption)){
+		console.log('SPAM!');
+		console.log(post.caption);
+		return true;
+	} else if(_.includes(spamAccounts, post.username)){
+		return true;
+	} else {
+		return false;
 	}
 }
 
