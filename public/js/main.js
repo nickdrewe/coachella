0d1ab722e303ea2075923be60696e82e564b4961
Nick Drewe <nick.drewe@gmail.com>
2015-03-28 13:53:15 +0700

    basic player

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 
-angular.module('myApp', ['ngAnimate'])
+angular.module('myApp', ['ngAnimate', 'angularSoundManager'])
+	.config([
+	    '$interpolateProvider', function($interpolateProvider) {
+	    	return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+	    }
+	])
 	.controller('MainCtrl', function($scope, $http){
 
 		$scope.data = {
@@ -12,6 +17,40 @@ angular.module('myApp', ['ngAnimate'])
 		});
 
 	})
+	.controller('SoundCtrl', ['$scope', function ($scope) {
+        $scope.songs = [
+            {
+                id: 'one',
+                title: 'Rain',
+                artist: 'Drake',
+                url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/rain.mp3'
+            },
+            {
+                id: 'two',
+                title: 'Walking',
+                artist: 'Nicki Minaj',
+                url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/walking.mp3'
+            },
+            {
+                id: 'three',
+                title: 'Barrlping with Carl (featureblend.com)',
+                artist: 'Akon',
+                url: 'http://www.freshly-ground.com/misc/music/carl-3-barlp.mp3'
+            },
+            {
+                id: 'four',
+                title: 'Angry cow sound?',
+                artist: 'A Cow',
+                url: 'http://www.freshly-ground.com/data/audio/binaural/Mak.mp3'
+            },
+            {
+                id: 'five',
+                title: 'Things that open, close and roll',
+                artist: 'Someone',
+                url: 'http://www.freshly-ground.com/data/audio/binaural/Things%20that%20open,%20close%20and%20roll.mp3'
+            }
+        ];
+    }])
 	.controller('CollageCtrl', ['$scope', '$window', function($scope, $window){
 
 		$scope.allImages = [];
