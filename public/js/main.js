568202e22f949c78be1de668122ed2c062bcc20c
Nick Drewe <nick.drewe@gmail.com>
2015-03-31 12:50:58 +0700

    player

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,5 @@
 
 angular.module('myApp', ['ngAnimate', 'angularSoundManager'])
-	.config([
-	    '$interpolateProvider', function($interpolateProvider) {
-	    	return $interpolateProvider.startSymbol('{(').endSymbol(')}');
-	    }
-	])
 	.controller('MainCtrl', function($scope, $http){
 
 		$scope.data = {
@@ -17,40 +12,32 @@ angular.module('myApp', ['ngAnimate', 'angularSoundManager'])
 		});
 
 	})
-	.controller('SoundCtrl', ['$scope', function ($scope) {
-        $scope.songs = [
-            {
-                id: 'one',
-                title: 'Rain',
-                artist: 'Drake',
-                url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/rain.mp3'
-            },
-            {
-                id: 'two',
-                title: 'Walking',
-                artist: 'Nicki Minaj',
-                url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/walking.mp3'
-            },
-            {
-                id: 'three',
-                title: 'Barrlping with Carl (featureblend.com)',
-                artist: 'Akon',
-                url: 'http://www.freshly-ground.com/misc/music/carl-3-barlp.mp3'
-            },
-            {
-                id: 'four',
-                title: 'Angry cow sound?',
-                artist: 'A Cow',
-                url: 'http://www.freshly-ground.com/data/audio/binaural/Mak.mp3'
-            },
-            {
-                id: 'five',
-                title: 'Things that open, close and roll',
-                artist: 'Someone',
-                url: 'http://www.freshly-ground.com/data/audio/binaural/Things%20that%20open,%20close%20and%20roll.mp3'
-            }
-        ];
-    }])
+	.controller('SoundCtrl', ['$scope', 'angularPlayer', function ($scope, angularPlayer) {
+		$scope.songs = [];
+		
+		SC.initialize({
+			client_id: "e1c2a174e148e7f9ef62cb7116c1c5fe"
+		});
+		
+		SC.get("/playlists/72999862/tracks", {
+			limit: 20
+		}, function(tracks) {
+			for (var i = 0; i < tracks.length; i ++) {
+				SC.stream( '/tracks/' + tracks[i].id, function( sm_object ){
+					var track = {
+						id: tracks[i].id,
+						title: tracks[i].title,
+						artist: tracks[i].genre,
+						url: sm_object.url
+					};
+					
+					$scope.$apply(function () {
+						$scope.songs.push(track);
+					});
+				});
+			}         
+		});
+	}])
 	.controller('CollageCtrl', ['$scope', '$window', function($scope, $window){
 
 		$scope.allImages = [];
--- public/lib/angular-soundmanager2/example-soundcloud/app.js
+++ public/lib/angular-soundmanager2/example-soundcloud/app.js
@@ -3,10 +3,12 @@ angular.module('myApp', ['angularSoundManager']).controller('MainCtrl', ['$scope
         $scope.songs = [];
         
         SC.initialize({
-            client_id: "YOUR-CLIENT-ID-HERE"
+            client_id: "e1c2a174e148e7f9ef62cb7116c1c5fe"
         });
         
-        SC.get("/groups/55517/tracks", {
+        // https://soundcloud.com/emzlogz/sets/coachella-2015
+        // curl -v 'http://api.soundcloud.com/resolve.json?url=https://soundcloud.com/emzlogz/sets/coachella-2015&client_id=e1c2a174e148e7f9ef62cb7116c1c5fe'
+        SC.get("/playlists/72999862/tracks", {
             limit: 5
         }, function(tracks) {
             for (var i = 0; i < tracks.length; i ++) {
